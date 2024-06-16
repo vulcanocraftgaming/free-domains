a92c38870ae4208147e62e95a5dbb5f67fcb2711
vulcanocraftgaming <vulcanocraftgaming@gmail.com>
2024-06-16 18:49:07 +0200

    Create vulcanotv.is-an.app.js

---
+++ domains/vulcanotv.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'an educational website',
+  domain: 'is-an.app',
+  subdomain: 'vulcanotv',
+  owner: {
+    repo: 'https://github.com/vulcanocraftgaming/free-domains',
+    email: 'vulcanocraftgaming@gmail.com',
+  },
+  record: {
+    CNAME: 'bit.ly/vulcanotv1',
+  },
+  proxy: false,
+})
